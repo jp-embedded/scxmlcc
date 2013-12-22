782c58cf8b340e0f8bd55cfe94a6dabadac356df
jp@jp-embedded.com <jp@jp-embedded.com@895cc7a6-dcaa-729c-b1c9-8451a495d010>
2013-12-22 23:28:34 +0000

    first test done

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,9 +1,15 @@
 #include "test355.h"
 #include <gtest/gtest.h>
 
-int main(int argc, char *argv[])
+TEST(w3c, 355)
 {
 	sc_test355 sc;
-	return 0;
+	EXPECT_EQ(&sc.m_state_pass, sc.cur_state);
+}
+
+int main(int argc, char *argv[])
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
 
