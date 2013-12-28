9fbe506eadad07b1582d5280a1699011955d17ca
jp@jp-embedded.com <jp@jp-embedded.com@895cc7a6-dcaa-729c-b1c9-8451a495d010>
2013-12-28 07:51:36 +0000

    added todo

--- trunk/src/version.cpp
+++ trunk/src/version.cpp
@@ -15,6 +15,8 @@
  ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *************************************************************************/
 
+//todo fix this in releases
+
 #ifndef VER
 #define VER "unknown"
 #endif
