b2b3a69ec9033e2057783a9d780c83d33ba098bc
jp@jp-embedded.com <jp@jp-embedded.com@895cc7a6-dcaa-729c-b1c9-8451a495d010>
2014-04-30 14:12:42 +0000

    test for expected result

--- branches/parallel/src/test/test.cpp
+++ branches/parallel/src/test/test.cpp
@@ -12,7 +12,8 @@ TEST(w3c, 576)
 {
 	sc_test576 sc;
 	sc.dispatch(&sc_test576::state::event_In_s11p112);
-//todo	EXPECT_EQ(&sc.m_state_pass, sc.cur_state);
+	EXPECT_EQ(1, sc.cur_state.size());
+	EXPECT_EQ(&sc.m_state_pass, sc.cur_state.front());
 }
 
 int main(int argc, char *argv[])
