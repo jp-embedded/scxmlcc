6760df5465e383933afed6a536ea5597c29666c3
jp@jp-embedded.com <jp@jp-embedded.com@895cc7a6-dcaa-729c-b1c9-8451a495d010>
2014-07-22 14:15:29 +0000

    initial use of sc

--- src/examples/vending_machine/main.cpp
+++ src/examples/vending_machine/main.cpp
@@ -15,6 +15,7 @@
  ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *************************************************************************/
 
+#include "vending_machine.h"
 #include "dispenser.h"
 #include "coin_refund.h"
 
