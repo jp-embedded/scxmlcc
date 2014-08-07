94ef6426e61efa9782fe435af494470dbcfcb6a9
jp@jp-embedded.com <jp@jp-embedded.com@895cc7a6-dcaa-729c-b1c9-8451a495d010>
2014-08-07 10:59:14 +0000

    connect first signals

--- src/examples/vending_machine/main.cpp
+++ src/examples/vending_machine/main.cpp
@@ -29,8 +29,15 @@ int main()
 	coin_sensor coin_sensor;
 	keypad keypad;
 
+	functor<sc_vending_machine::event> dispatch(&sc, &sc_vending_machine::dispatch);
+
 	input.sig_key.push_back(functor<const char>(&coin_sensor, &coin_sensor::simulate_input));
 	input.sig_key.push_back(functor<const char>(&keypad, &keypad::simulate_input));
+	coin_sensor.sig_dime.push_back(bind(dispatch, &sc_vending_machine::state::event_D));
+	coin_sensor.sig_nickel.push_back(bind(dispatch, &sc_vending_machine::state::event_N));
+	keypad.sig_coke.push_back(bind(dispatch, &sc_vending_machine::state::event_coke));
+	keypad.sig_diet.push_back(bind(dispatch, &sc_vending_machine::state::event_diet));
+//todo	keypad.sig_zero.push_back(bind(dispatch, &sc_vending_machine::state::event_zero));
 
 	input.run();
 
