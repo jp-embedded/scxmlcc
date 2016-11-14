072091ffd45e9e3a1f594ff5042603990ea744ac
Jan Pedersen <jp@jp-embedded.com>
2016-11-14 15:57:09 +0000

    add logging

--- src/examples/microwave.cpp
+++ src/examples/microwave.cpp
@@ -16,17 +16,68 @@
  *************************************************************************/
 
 #include "microwave-01-cplusplus.h"
+#include "termio.h"
 #include <iostream>
 
 using namespace std;
 
 typedef sc_microwave_01_cplusplus sc;
 
+// add logging
+template<> void sc::state_actions<sc::state_off>::enter(data_model &m)
+{
+	cout << "off" << endl;
+}
+
+template<> void sc::state_actions<sc::state_on>::enter(data_model &m)
+{
+	cout << "on" << endl;
+}
+
+template<> void sc::state_actions<sc::state_idle>::enter(data_model &m)
+{
+	cout << "idle" << endl;
+}
+
+template<> void sc::state_actions<sc::state_cooking>::enter(data_model &m)
+{
+	cout << "cooking" << endl;
+}
+
 int main(int argc, char *argv[])
 {
+	// set unbuffered input and disable echo
+	termio t;
+	t.echo(false);
+	t.canonical(false);
+
+	cout << "input:  P = turn on" << endl;
+      	cout << "        p = turn off" << endl;
+      	cout << "        D = open door" << endl;
+      	cout << "        d = close door" << endl;
+	cout << "        t = timer" << endl;
+	cout << "        q = quit" << endl;
+
 	sc sc;
 	sc.init();
 
+	while(true) {
+		char c;	
+		cin >> c;
+
+		sc::event e;
+		switch (c) {
+			case 'P': e = &sc::state::event_turn_on;    break;
+			case 'p': e = &sc::state::event_turn_off;   break;
+			case 'D': e = &sc::state::event_door_open;  break;
+			case 'd': e = &sc::state::event_door_close; break;
+			case 't': e = &sc::state::event_time;       break;
+			case 'q': return 0;
+			default: continue;
+		}
+		sc.dispatch(e);
+	}
+	
 	return 0;
 }
 
