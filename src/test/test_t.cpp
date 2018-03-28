e4ef8eefab02dce97267289375eb7a598e37540a
Jan Pedersen <jp@jp-embedded.com>
2018-03-28 22:29:29 +0200

    fix test

--- src/test/test_t.cpp
+++ src/test/test_t.cpp
@@ -1,3 +1,5 @@
+#if __cplusplus > 201703L
+
 #include "test144_t.h"
 #include "test355_t.h"
 #include "test364_t.h"
@@ -20,6 +22,46 @@
 #include <chrono>
 
 namespace threadsafe {
+
 #include "all-tests.h"
+
+TEST(timer_switch, 1)
+{
+	sc_timer_switch::user_model m;
+	sc_timer_switch sc(&m);
+	sc.init();
+
+	EXPECT_EQ(typeid(sc_timer_switch::state_off), typeid(*sc.cur_state));
+
+	std::thread t([&sc]{ sc.dispatch_loop(); });
+	std::this_thread::sleep_for(10ms); // Give time for dispatch_loop to be entered...
+
+	sc.model.push_event(&sc_timer_switch::state::event_button);
+	std::this_thread::sleep_for(10ms);
+	EXPECT_EQ(typeid(sc_timer_switch::state_on), typeid(*sc.cur_state));
+	sc.model.push_event(&sc_timer_switch::state::event_button);
+	std::this_thread::sleep_for(10ms);
+	EXPECT_EQ(typeid(sc_timer_switch::state_off), typeid(*sc.cur_state));
+	sc.model.push_event(&sc_timer_switch::state::event_button);
+
+	sc.model.cancel_wait();
+	t.join();
+
+	EXPECT_EQ(typeid(sc_timer_switch::state_on), typeid(*sc.cur_state));
+
+	sc.model.push_event(&sc_timer_switch::state::event_timer);
+	sc.model.push_event(&sc_timer_switch::state::event_timer);
+	sc.model.push_event(&sc_timer_switch::state::event_timer);
+	sc.model.push_event(&sc_timer_switch::state::event_timer);
+	sc.model.push_event(&sc_timer_switch::state::event_timer);
+
+	auto e = sc.model.pop_event();
+	if (e) sc.dispatch(*e);
+
+	EXPECT_EQ(typeid(sc_timer_switch::state_off), typeid(*sc.cur_state));
 }
+
+}
+
+#endif
 
