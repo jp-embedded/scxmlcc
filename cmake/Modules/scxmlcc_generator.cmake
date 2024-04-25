d5380abf204094d9056285106be162104c872f8f
Marcel Behlau <marcel.behlau@elfin.de>
2024-04-25 15:01:34 +0200

    style: remove whitespaces at line end

--- cmake/Modules/scxmlcc_generator.cmake
+++ cmake/Modules/scxmlcc_generator.cmake
@@ -6,22 +6,22 @@ function (scxmlcc_generator filename gen-list )
    else()
       find_program( SCC scxmlcc )
    endif()
-   
+
    get_filename_component(ext ${filename} EXT)
    if(NOT ext STREQUAL ".scxml")
-      message("skipping ${filename}") 
+      message("skipping ${filename}")
       return()
    endif()
 
    get_filename_component(base ${filename} NAME_WE)
    set(output ${CMAKE_CURRENT_BINARY_DIR}/${base}.h)
-   
+
    set_source_files_properties(${output} PROPERTIES GENERATED TRUE)
    set(${gen-list} ${${gen-list}} ${output} PARENT_SCOPE)
-   
+
    add_custom_command(
         OUTPUT ${output}
-        COMMAND ${SCC} --stringevents -i ${filename}  -o ${output} 
+        COMMAND ${SCC} --stringevents -i ${filename}  -o ${output}
         DEPENDS ${filename}
    )
 
