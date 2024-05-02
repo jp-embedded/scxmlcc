8bb0a32efa6e30090d7eb8ff2e040c612a06975b
Marcel Behlau <marcel.behlau@elfin.de>
2024-05-02 12:04:29 +0200

    feature: introduce cmake_parse_arguments within scxmlcc_generator

--- cmake/Modules/scxmlcc_generator.cmake
+++ cmake/Modules/scxmlcc_generator.cmake
@@ -2,6 +2,11 @@
 # Usage:
 #   INPUTS: Input file(s)
 #   TARGETS: Name of target(s), which depends on the statemachine
+#
+# The generated file(s) are placed within current build folder. Every input
+# file will produce an output file. The output name depends on the input name,
+# eg. microwave-01-cplusplus.xml --> microwave-01-cplusplus.h
+
 function (scxmlcc_generator )
 
   set(options "")
@@ -22,31 +27,37 @@ function (scxmlcc_generator )
       find_program( SCC scxmlcc )
    endif()
 
-   get_filename_component(ext ${filename} EXT)
-   if(NOT ext STREQUAL ".scxml")
-      message("skipping ${filename}")
-      return()
-   endif()
+   foreach(input ${PARSED_ARG_INPUTS})
+     get_filename_component(ext ${input} EXT)
+     if(NOT ext STREQUAL ".scxml")
+        message("skipping ${input}")
+        continue()
+     endif()
 
-   get_filename_component(base ${filename} NAME_WE)
-   set(output ${CMAKE_CURRENT_BINARY_DIR}/${base}.h)
+     get_filename_component(base ${input} NAME_WE)
+     set(output ${CMAKE_CURRENT_BINARY_DIR}/${base}.h)
 
-   add_custom_command(
-        OUTPUT ${output}
-        COMMAND ${SCC} --stringevents -i ${filename}  -o ${output}
-        DEPENDS ${filename}
-   )
+     add_custom_command(
+          OUTPUT ${output}
+          COMMAND ${SCC} --stringevents -i ${input}  -o ${output}
+          DEPENDS ${input}
+     )
+     list(APPEND outputs ${output})
+   endforeach()
 
    # Append hash to avoid problems with same named input files
-   string(MD5 hash ${filename})
+   string(MD5 hash "${PARSED_ARG_INPUTS}")
    set(intermediateTarget "scxmlcc_${base}_${hash}")
 
    add_custom_target(
         ${intermediateTarget}
-        DEPENDS ${output}
+        DEPENDS ${outputs}
    )
 
-   add_dependencies(${target} ${intermediateTarget})
+   foreach(target ${PARSED_ARG_TARGETS})
+      add_dependencies(${target} ${intermediateTarget})
+   endforeach()
+
 
 
 endfunction()
